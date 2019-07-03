521865f061f7d14b7821815b856906e29ad3bfe2
venkatbhimisetty <bhimisettyvenkatasubbarao@gmail.com>
2019-07-03 13:04:17 +0000

    content change

--- hello.js
+++ hello.js
@@ -2,6 +2,6 @@
 var http = require('http');
 http.createServer(function (req, res) {
           res.writeHead(200, {'Content-Type': 'text/plain'});
-          res.end('Hello World, installation done using anisble\n');
+          res.end('Hello World, installation done using anisble and jenkins\n');
 }).listen(3333, 'localhost');
 console.log('Server running at http://localhost:3333/');
